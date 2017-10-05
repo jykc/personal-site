875d3b7546ec5d9e13ab2afbfd3cad47487a1aa2
Jessica Choong <jchoongnw@gmail.com>
2017-10-05 01:50:59 +0100

    navbar links and artwork images cropped

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,24 @@
 class PagesController < ApplicationController
+
   def home
   end
 
   def artwork
+    @images = Image.all
   end
 
   def photos
+
   end
+
+  private
+
+  def set_image
+    @image = Image.find(params[:id])
+  end
+
+  def image_params
+    params.require(:image).permit(:name, :photo, :photo_cache)
+  end
+
 end
