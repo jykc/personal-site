18c87371305727ca653166d5da61187b205695d2
Jessica Choong <jchoongnw@gmail.com>
2017-10-16 16:40:08 +0100

    got modal working for 1st image, added scrolling and backtotop button

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'artwork', to: "pages#artwork"
   get 'photos', to: "photos#index"
+  get 'contact', to: "pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
