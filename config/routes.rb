ccb335a8da8b10a935e6bcb0e13b4f234e3982d2
Jessica Choong <jchoongnw@gmail.com>
2018-02-12 14:32:03 +0000

    carousel modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'artwork', to: "pages#artwork"
-  get 'photos', to: "photos#index"
+  get 'photos', to: "pages#photos"
+  get 'owls', to: "pages#owls"
+  get 'flowers', to: "pages#flowers"
+  get 'streetart', to: "pages#streetart"
+  get 'thailand', to: "pages#thailand"
   get 'contact', to: "pages#contact"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
