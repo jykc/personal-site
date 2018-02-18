52c42781a991af2e0a4680b8e941475765aed65f
Jessica Choong <jchoongnw@gmail.com>
2018-02-18 11:46:24 +0000

    lily links added

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'streetart', to: "pages#streetart"
   get 'thailand', to: "pages#thailand"
   get 'contact', to: "pages#contact"
+  get 'lily', to: "pages#lily"
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
